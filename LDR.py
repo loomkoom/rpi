340ff206fead94ba24f300c6eccfb2d1345e746e
loomkoom <29258685+loomkoom@users.noreply.github.com>
2022-03-22 12:45:48 +0100

    LDR examples

--- LDR.py
+++ LDR.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 
-while True:
-    if (GPIO.input(17)==0):
-        print("Dark")
-        time.sleep(0.5)
-    else:
-        GPIO.output(18,0)
-        print("Light")
-        time.sleep(0.5)
-        
+
+try:
+    while True:
+        if (GPIO.input(17) == 0):
+            print("Dark")
+            time.sleep(0.5)
+        else:
+            print("Light")
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nProgram Exited")
+    exit(0)
