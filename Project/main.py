d0d28b5a3d1354f92034430b0510f53c40729e35
loomkoom <29258685+loomkoom@users.noreply.github.com>
2022-04-26 15:52:19 +0200

    project week 2

--- Project/main.py
+++ Project/main.py
@@ -1,39 +1,87 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
-from datetime import datetime
+from asyncio import start_server
+from adafruit_bus_device.spi_device import SPIDevice
+import spidev
+import busio
+import digitalio
+import board
+import adafruit_pcd8544
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import cgitb
+cgitb.enable()
 GPIO.setmode(GPIO.BCM)
 
 DISTANCE_TRESHOLD = 30
-LAMP_ON = 8
-LAMP_OFF = 20
-isLampOn = False;
+LAMP_ON = "15:38"
+LAMP_OFF = "15:39"
+isLampOn = False
+hasFed = False
 
-trigPinOut = 24
-echoPinIn = 25
-GPIO.setup(trigPinOut, GPIO.IN)
-GPIO.setup(echoPinIn, GPIO.OUT)
+trigPinOut = 25
+echoPinIn = 24
+GPIO.setup(trigPinOut, GPIO.OUT)
+GPIO.setup(echoPinIn, GPIO.IN)
 GPIO.output(trigPinOut, 0)
 
 rstPin = 6
 dcPin = 5
 
+pumpPin = 13
+GPIO.setup(pumpPin, GPIO.OUT)
+GPIO.output(pumpPin, 1)
+
 ledPin = 22
 GPIO.setup(ledPin, GPIO.OUT)
 
-stepPins = [12,16,20,21]
+
+# Initialize SPI bus
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Initialize control pins for adc
+cs0 = digitalio.DigitalInOut(board.CE0)  # chip select
+adc = SPIDevice(spi, cs0, baudrate=1000000)
+
+# Initialize display
+dc = digitalio.DigitalInOut(board.D6)  # data/command
+cs1 = digitalio.DigitalInOut(board.CE1)  # chip select CE1 for display
+reset = digitalio.DigitalInOut(board.D5)  # reset
+display = adafruit_pcd8544.PCD8544(spi, dc, cs1, reset, baudrate=1000000)
+display.bias = 4
+display.contrast = 60
+display.invert = True
+
+#  Clear the display.  Always call show after changing pixels to make the display update visible!
+display.fill(0)
+display.show()
+
+# Load default font.
+font = ImageFont.load_default()
+font = ImageFont.truetype(
+    "/usr/share/fonts/truetype/freefont/FreeSansBold.ttf", 18)
+
+# Get drawing object to draw on image
+image = Image.new('1', (display.width, display.height)
+                  )  # mode 1 is for monochrome
+draw = ImageDraw.Draw(image)
+
+
+stepPins = [12, 16, 20, 21]
 for pin in stepPins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
 
 btnPins = [23, 4, 17, 27]
+btnvals = [0, 0, 0, 0]
 for pin in btnPins:
-    GPIO.setup(pin, GPIO.INPUT)
-
-pumpPin = 13
+    GPIO.setup(pin, GPIO.IN)
 
 
 time.sleep(1)
+
 
 def pair(pin_on, pin_off):
     GPIO.output(pin_on, 1)
@@ -47,7 +95,19 @@ def full_step(seq):
         pair(pin_on, pin_off)
         time.sleep(0.01)
         i += 1
+        
+def motor_on(direction):
+    if direction == "forwards":
+        full_step(stepPins + stepPins[0:2])
+    if direction == "backwards":
+        full_step((stepPins + stepPins[0:2])[::-1])
 
+
+def motor_off():
+    GPIO.output(stepPins[0], 0)
+    GPIO.output(stepPins[1], 0)
+    GPIO.output(stepPins[2], 0)
+    GPIO.output(stepPins[3], 0)
 
 def measure_distance():
     GPIO.output(trigPinOut, 1)
@@ -68,74 +128,114 @@ def measure_distance():
 
 try:
     while True:
-        # manual buttons 
-        if (GPIO.input(btnPins[0]) == 0):
-            print("Light turns on")
-            GPIO.output(ledPin,1)
-            isLampOn = True;
-            time.sleep(0.5)
+        # manual buttons
+        if (GPIO.input(btnPins[0]) == 1):
+            if btnvals[0] == 0:
+                print("Light turns on")
+                btnvals[0] = 1
+                GPIO.output(ledPin, 1)
+                isLampOn = True
+                time.sleep(0.5)
         else:
-            print("Light turns off")
-            GPIO.output(ledPin,0)
-            isLampOn = False;
-            time.sleep(0.5)
+            if btnvals[0] == 1:
+                print("Light turns off")
+                btnvals[0] = 0
+                GPIO.output(ledPin, 0)
+                isLampOn = False
+                time.sleep(0.5)
 
-        if (GPIO.input(btnPins[1]) == 0):
-            print("pump on")
-            GPIO.output(pumpPin,1)
-            time.sleep(0.5)
+        if (GPIO.input(btnPins[1]) == 1):
+            if btnvals[1] == 0:
+                btnvals[1] = 1
+                print("pump on")
+                GPIO.output(pumpPin, 0)
+                time.sleep(0.5)
         else:
-            print("pump off")
-            GPIO.output(pumpPin,0)
-            time.sleep(0.5)
+            if btnvals[1] == 1:
+                print("pump off")
+                btnvals[1] = 0
+                GPIO.output(pumpPin, 1)
+                time.sleep(0.5)
 
-        if (GPIO.input(btnPins[2]) == 0):
-            print("stepper forward")
-            full_step(stepPins + stepPins[0:2])
-            time.sleep(0.5)
+        if (GPIO.input(btnPins[2]) == 1):
+            if btnvals[2] == 0:
+                btnvals[2] = 1
+                print("stepper forward")
+                stepper_on = time.time()
+                while time.time() - stepper_on < 3:
+                    motor_on("forwards")               
         else:
-            print("stepper off")
-            GPIO.output(stepPins[0],0)
-            GPIO.output(stepPins[1],0)
-            GPIO.output(stepPins[2],0)
-            GPIO.output(stepPins[3],0)
-            time.sleep(0.5)
+            if btnvals[2] == 1:
+                print("stepper off")
+                btnvals[2] = 0
+                motor_off()
 
-        if (GPIO.input(btnPins[3]) == 0):
-            print("stepper backward")
-            full_step((pins + pins[0:2])[::-1])
-            time.sleep(0.5)
+        if (GPIO.input(btnPins[3]) == 1):
+            if btnvals[3] == 0:
+                btnvals[3] = 1
+                print("stepper backward")
+                stepper_on = time.time()
+                while time.time() - stepper_on < 3:
+                    motor_on("backwards")     
         else:
-            print("stepper off")
-            GPIO.output(stepPins[0],0)
-            GPIO.output(stepPins[1],0)
-            GPIO.output(stepPins[2],0)
-            GPIO.output(stepPins[3],0)
-            time.sleep(0.5)
+            if btnvals[3] == 1:
+                print("stepper off")
+                btnvals[3] = 0
+                motor_off()
+                
         # automatic light
-        time = time.strftime("%H:%M",time.localtime())
-        if not isLampOn and time > LAMP_ON  :
-            GPIO.output(ledPin,1)
-            isLampOn = True;
-        if not  isLampOn and time > LAMP_OFF:
-            GPIO.output(ledPin,0)
+        human_time = time.strftime("%H:%M", time.localtime())
+        if not isLampOn and human_time >= LAMP_ON:
+            GPIO.output(ledPin, 1)
+            isLampOn = True
+        if isLampOn and human_time >= LAMP_OFF:
+            GPIO.output(ledPin, 0)
             isLampOn = False
+            
+        # feed at set time:
+        if not hasFed and human_time >= FEED_TIME:
+            draw.rectangle((0, 0, display.width, display.height),
+                       outline=255, fill=255)
+            draw.text((1, display.height//2), "FEEDING", font=font)
+            display.image(image)
+            display.show()
+            motor_on("forwards")
+            isLampOn = True
+        if hasFed and human_time >= FEED_TIME:
+            GPIO.output(ledPin, 0)
+            isLampOn = False       
+            
 
         # pump when water too low
-        distance = round(measure_distance(),2)
+        distance = round(measure_distance(), 2)
 
         if distance > DISTANCE_TRESHOLD:
-            GPIO.output(stepPins[0],0)
-            GPIO.output(stepPins[1],0)
-            GPIO.output(stepPins[2],0)
-            GPIO.output(stepPins[3],0)
+            motor_off()
             print('safe: ', distance, "cm")
         else:
+            draw.rectangle((0, 0, display.width, display.height),
+                       outline=255, fill=255)
+            draw.text((1, display.height//2), "PUMPING", font=font)
+            display.image(image)
+            display.show()
             while distance < DISTANCE_TRESHOLD:
                 print('alarm: ', distance, "cm")
-                GPIO.output(ledPin,1)
-                full_step(stepPins + stepPins[0:2])
-                distance = round(measure_distance(),2)
+                motor_on("forwards")
+                distance = round(measure_distance(), 2)
+
+        distance = round(measure_distance(), 2)
+
+        draw.rectangle((0, 0, display.width, display.height),
+                       outline=255, fill=255)
+
+        draw.text((1, 0), human_time, font=font)
+        draw.text((1, display.height//4*1), str(isLampOn), font=font)
+        draw.text((1, display.height//4*2),
+                  str(distance), font=font)
+        draw.text((1, display.height//4*3), "fill", font=font)
+
+        display.image(image)
+        display.show()
         time.sleep(0.1)
 
 
